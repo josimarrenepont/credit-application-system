75d693914b4299491e097ce53447813a65e4282e
josimarrenepont <jnrenepont@hotmail.com>
2023-12-23 23:39:59 -0300

    Created CreditController - postMapping method

--- src/main/kotlin/me/dio/credit/request/system/controllers/CreditController.kt
+++ src/main/kotlin/me/dio/credit/request/system/controllers/CreditController.kt
@@ -1,4 +1,21 @@
 package me.dio.credit.request.system.controllers
 
-class CreditController {
+import me.dio.credit.request.system.dto.CreditDTO
+import me.dio.credit.request.system.entity.Credit
+import me.dio.credit.request.system.services.impl.CreditService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/credits")
+class CreditController(
+        private val creditService: CreditService
+) {
+    @PostMapping
+    fun saveCredit(@RequestBody creditDTO: CreditDTO): String{
+        val credit: Credit =  this.creditService.save(creditDTO.toEntity())
+        return "Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved!"
+    }
 }
