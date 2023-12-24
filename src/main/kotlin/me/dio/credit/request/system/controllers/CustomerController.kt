c390df06602a1164bf88981762a473881bc135b3
josimarrenepont <jnrenepont@hotmail.com>
2023-12-23 22:25:12 -0300

    Created delete method

--- src/main/kotlin/me/dio/credit/request/system/controllers/CustomerController.kt
+++ src/main/kotlin/me/dio/credit/request/system/controllers/CustomerController.kt
@@ -1,9 +1,15 @@
 package me.dio.credit.request.system.controllers
 
 import me.dio.credit.request.system.dto.CustomerDTO
+import me.dio.credit.request.system.dto.CustomerView
+import me.dio.credit.request.system.entity.Customer
 import me.dio.credit.request.system.services.impl.CustomerService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -20,5 +26,14 @@ class CustomerController(
     fun saveCustomer(@RequestBody customerDTO: CustomerDTO): String{
         val savedCustomer = this.customerService.save(customerDTO.toEntity())
         return "Customer ${savedCustomer.email} saved!"
+    }
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): CustomerView {
+        val customer: Customer = this.customerService.findById(id)
+        return CustomerView(customer)
+    }
+    @DeleteMapping("/{id}")
+    fun deleteCustomer(@PathVariable id: Long){
+        this.customerService.delete(id)
     }
 }
